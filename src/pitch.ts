c2fd9029ca36ad1b63967988955a8efa0d03fbeb
LUO Sheng <luosheng1986@gmail.com>
2021-11-04 23:05:24 +0800

    Convert pitch to string

--- src/pitch.ts
+++ src/pitch.ts
@@ -2,5 +2,9 @@ export type Tune = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G'
 export type Octave = number
 
 export class Pitch {
-  constructor(public readonly tune: Tune, public readonly octave: Octave) {}
+  constructor(public readonly tune: Tune, public readonly octave: Octave) { }
+
+  toString(): string {
+    return `${this.tune}${this.octave}`
+  }
 }
