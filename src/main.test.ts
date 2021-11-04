7b7e61db17f0154b57d487c3ae5e8686495e254b
LUO Sheng <luosheng1986@gmail.com>
2021-11-04 23:17:20 +0800

    Pitch test with accidental

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,10 +1,18 @@
-import { Pitch } from './pitch'
+import { Accidental, Pitch } from './pitch'
 
 test('pitch creation', () => {
   const pitch = new Pitch('C', 4)
   expect(pitch.tune).toBe('C')
   expect(pitch.octave).toBe(4)
   expect(pitch.toString()).toBe('C4')
+})
+
+test('pitch creation with accidentail', () => {
+  const pitchSharp = new Pitch('C', 4, Accidental.Sharp)
+  expect(pitchSharp.toString()).toBe('C♯4')
+
+  const pitchFlat = new Pitch('C', 4, Accidental.Flat)
+  expect(pitchFlat.toString()).toBe('C♭4')
 })
 
 test('pitch decomposition', () => {
